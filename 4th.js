7a0758c5c5e80e8a1384f87d92bad1380f3a0a3f
Shreevatsatg <tgshreevatsa@gmail.com>
2024-07-31 18:08:19 +0530

    added practice questions to 4th.js

--- 4th.js
+++ 4th.js
@@ -1,12 +1,27 @@
 //array
-let marks =[12,34,56,78,839,];
-console.log(marks[0]);
-console.log(marks.length);
-marks[0]=10;
-console.log(marks[0]);
-for(let i=0;i<5;i++){
-    console.log(marks[i]);
-}
+// let marks =[12,34,56,78,839];
+// console.log(marks[0]);
+// console.log(marks.length);
+// marks[0]=10;
+// console.log(marks[4]);
+// for(let i=0;i<marks.length;i++){
+//     console.log(marks[i]);
+// }
+// //useing for of loop
+// for(let el of marks){
+//     console.log(el);
+// }
+
+//1practice question
+// let a=[85,97,44,37,76,60]
+// let sum=0;
+// for (let i of a){//using for of loop
+//     sum+=i;
+//     }
+// let avg =sum/a.length;
+// console.log(`average marks of a class is = ${avg}`);//useing templete literals
+
+//2practice question
 
 
 
