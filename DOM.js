c65055bcf31804e0dfd0c1e10f0ddf24297ed0ea
Shreevatsatg <tgshreevatsa@gmail.com>
2024-08-01 20:08:56 +0530

    added practice questions to dom.js

--- DOM.js
+++ DOM.js
@@ -3,11 +3,17 @@
 //document.getElementbyid //to select elememt by id
 //document.getElementsBytag   //to select a element by tag name
 //document.querryselector ??to select anything that matches the element name given
-let a=document.querySelector("p")
-console.dir(a);//dir to display dom 
-let b=document.querySelectorAll("p")//to get all matching outputs
+// let a=document.querySelector("p")
+// console.dir(a);//dir to display dom 
+// let b=document.querySelectorAll("p")//to get all matching outputs
 
-console.dir(b);//dir to display dom 
+// console.dir(b);//dir to display dom 
+
+//practice questions
+
+// let h2=document.querySelector("h2");
+// console.dir(h2.innertext);
+// h2.innertext = h2.innertext+"from shreevatsa tg";//added two string
 
 
 
