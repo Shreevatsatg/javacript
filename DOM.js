fadf52d85c943317491841c241664ce3d760bd44
Shreevatsatg <tgshreevatsa@gmail.com>
2024-08-01 19:40:49 +0530

    created dom.js dom.css dom.html to practice dom concepts

---
+++ DOM.js
@@ -0,0 +1,13 @@
+// console.log("hello");
+// let heading = document.getElementsByClassName("heading");//h1//to select by classname
+//document.getElementbyid //to select elememt by id
+//document.getElementsBytag   //to select a element by tag name
+//document.querryselector ??to select anything that matches the element name given
+let a=document.querySelector("p")
+console.dir(a);//dir to display dom 
+let b=document.querySelectorAll("p")//to get all matching outputs
+
+console.dir(b);//dir to display dom 
+
+
+
