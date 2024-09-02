459c9dfcbb9504040ffe4106ecf02f9869a54c96
Shreevatsatg <tgshreevatsa@gmail.com>
2024-09-02 12:19:57 +0530

    added callback practice files

---
+++ callback.js
@@ -0,0 +1,12 @@
+console.log("one");
+console.log("two");
+
+// function hello(){
+// console.log("hellow");
+// }
+// setTimeout(hello,2000)//to delay the output 2sec= 2000 milisec
+setTimeout(() => {console.log("two and half"); 
+},4000);
+
+console.log("three");
+console.log("four");
