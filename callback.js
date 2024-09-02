4287b5eb6769506ce5e375019f62facda3456eb6
Shreevatsatg <tgshreevatsa@gmail.com>
2024-09-02 19:41:49 +0530

    added api practice file and upgraded call back

--- callback.js
+++ callback.js
@@ -5,15 +5,15 @@
 // });
 
 //  console.log("one");
-//console.log("two");
+// console.log("two");
 
 // function hello(){
 // console.log("hellow");
 // }
 // setTimeout(hello,2000)//to delay the output 2sec= 2000 milisec
-//setTimeout(() => {console.log("two and half"); 
-//},4000);
+// setTimeout(() => {console.log("two and half"); 
+// },4000);
 
-//console.log("three");
-//console.log("four");
+// console.log("three");
+// console.log("four");
 
