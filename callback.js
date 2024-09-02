c5f84fc591085bbb85f7f9ccbce36c224261b034
Shreevatsatg <tgshreevatsa@gmail.com>
2024-09-02 12:48:05 +0530

    added promises practice

--- callback.js
+++ callback.js
@@ -1,12 +1,19 @@
-console.log("one");
-console.log("two");
+// let promise = new promise ((resolve,reject) => {
+//     console.log("i am a promise ");
+//     resolve(123);
+//     reject(234);
+// });
+
+//  console.log("one");
+//console.log("two");
 
 // function hello(){
 // console.log("hellow");
 // }
 // setTimeout(hello,2000)//to delay the output 2sec= 2000 milisec
-setTimeout(() => {console.log("two and half"); 
-},4000);
+//setTimeout(() => {console.log("two and half"); 
+//},4000);
 
-console.log("three");
-console.log("four");
+//console.log("three");
+//console.log("four");
+
